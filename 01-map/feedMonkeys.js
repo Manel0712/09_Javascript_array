89327880e557643928f5fc59dd838c84223de815
Manel <maneldelahozrodriguez@gmail.com>
2024-01-11 09:28:59 +0100

    Completed imperative exercies

--- 01-map/feedMonkeys.js
+++ 01-map/feedMonkeys.js
@@ -7,3 +7,12 @@
  */
 
 const MONKEYS = ["🐒", "🦍", "🦧"];
+
+const feedMonkeys = function(fruit) {
+    const monkeysFruit = MONKEYS.map((element)=>{
+        return `${element} ${fruit}`
+    })
+    return monkeysFruit
+}
+
+export default feedMonkeys;
